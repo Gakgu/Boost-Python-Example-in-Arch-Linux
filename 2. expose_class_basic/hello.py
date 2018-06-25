2e5acd57c091de50196b9d44faffa8da2ee71a02
gakgu <serenad@naver.com>
2018-06-25 07:35:51 +0000

    rm

--- 2. expose_class_basic/hello.py
+++
@@ -1,4 +0,0 @@
-import hello
-planet = hello.World()
-planet.set('howdy')
-print(planet.greet())
--- 3. expose_class_explicit_constructors/hello.py
+++
@@ -1,7 +0,0 @@
-import hello
-
-expose_1_constructor_class = hello.expose_1_constructor("hello")
-expose_2_constructor_class = hello.expose_2_constructor(1234, 4321)
-
-print(expose_1_constructor_class.greet())
-print(expose_2_constructor_class.sum())
